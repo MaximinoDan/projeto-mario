db1df22b35701d9b62985214c7930135e6c25f36
Daniel Maximino <97485733+MaximinoDan@users.noreply.github.com>
2023-01-22 16:03:48 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.src;  
+
+function altenarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    console.log("clicou no botão de veja o trailer");
+    altenarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    altenarModal();
+    video.setAttribute("src" , "");
+});
+
